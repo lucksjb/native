29786400b4ef24a82ebed2501200b6f22c517c7c
Luciano D. Silva <lucksjb@gmail.com>
2022-01-05 11:23:21 -0300

    initial commit

---
+++ src/main/java/com/example/native2/Native2Application.java
@@ -0,0 +1,13 @@
+package com.example.native2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Native2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Native2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/native2/Native2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.native2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Native2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
